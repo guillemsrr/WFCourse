48ef538182e14c82583d77609bd07a9b550e0a37
Guillem Serra <guillemserralorenz@gmail.com>
2022-09-23 09:42:03 +0200

    constant rotator class

---
+++ Assets/Scripts/UI/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace WFCourse.UI
+{
+    public class Rotator: MonoBehaviour
+    {
+        [SerializeField] private Transform _transform;
+        [SerializeField] private float _speed = 1f;
+
+        private void Update()
+        {
+            _transform.Rotate(Vector3.up, _speed*Time.deltaTime);
+        }
+    }
+}
